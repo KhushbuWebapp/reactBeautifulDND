ef96ff5d6b93c3abf9cb74e3ded1ec5e18f03dcc
KhushbuWebapp <khushbu.webappdev@gmail.com>
2024-09-03 10:28:55 +0530

    add new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import List_DND from "./components/List_DND";
 
 function App() {
-  return <>React Beautiful DND</>;
+  return (
+    <>
+      <List_DND />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/List_DND.jsx
@@ -0,0 +1,16 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+const List_DND = () => {
+  return (
+    <>
+      <Box>
+        <Typography variant="h2">Draggable List</Typography>
+
+        {/* <DraggableList /> */}
+      </Box>
+    </>
+  );
+};
+
+export default List_DND;
